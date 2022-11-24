7527f977081974539d53b8e59a84258fd98c4123
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-11-24 23:01:04 +0100

    style: info bar linear gradient background

--- lib/reused_widgets/info_bar.dart
+++ lib/reused_widgets/info_bar.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/responsive_layout.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:portfolio_website/reused_widgets/icon_tile.dart';
-import 'package:flutter_devicon/flutter_devicon.dart';
 
 class InfoBar extends StatelessWidget {
   const InfoBar({
@@ -17,7 +14,11 @@ class InfoBar extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
-      color: Colors.blueGrey,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.blueAccent, Colors.blueGrey],
+        ),
+      ),
       child: Padding(
         padding: EdgeInsets.symmetric(
           vertical: ResponsiveLayout.isDesktop(context) ? 20 : 10,
@@ -25,29 +26,6 @@ class InfoBar extends StatelessWidget {
               ResponsiveLayout.isDesktop(context) ? screenWidth * 0.12 : 10,
         ),
         child: body,
-        // child: Row(
-        //   children: [
-        //     Expanded(
-        //       child: Column(
-        //         crossAxisAlignment: CrossAxisAlignment.start,
-        //         children: [
-        //           const Text(
-        //             "Tech stack",
-        //             style: TextStyle(
-        //               fontWeight: FontWeight.bold,
-        //               color: Colors.white,
-        //               fontSize: 30,
-        //             ),
-        //           ),
-        //           const SizedBox(
-        //             height: 20,
-        //           ),
-        //           Wrap(spacing: 15, runSpacing: 15, children: []),
-        //         ],
-        //       ),
-        //     ),
-        //   ],
-        // ),
       ),
       // child: ,
     );
