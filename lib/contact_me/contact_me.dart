a37be51576d5c1f5f763158c1cd98bcbccb19e22
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-11-25 09:45:54 +0100

    add: opening social accounts on tap in another tab (with url_launcher package)

--- lib/contact_me/contact_me.dart
+++ lib/contact_me/contact_me.dart
@@ -1,11 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/responsive_layout.dart';
 import 'package:portfolio_website/reused_widgets/icon_tile.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio_website/reused_widgets/info_bar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactMe extends StatelessWidget {
   const ContactMe({Key? key}) : super(key: key);
+
+  Future<void> _launchUrl(String url) async {
+    final uri = Uri.parse(url);
+    if (kDebugMode) {
+      print(uri.isScheme('https'));
+    }
+    if (!await launchUrl(uri)) {
+      print("problem");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +49,39 @@ class ContactMe extends StatelessWidget {
             spacing: 15,
             runSpacing: 15,
             children: [
-              IconTile(
-                showText: ResponsiveLayout.isDesktop(context),
-                icon: FontAwesomeIcons.linkedin,
-                iconColor: Colors.white,
-                text: "Linkedin",
-                textColor: Colors.white,
-                backgroundColor: Colors.blueAccent,
+              GestureDetector(
+                onTap: () =>
+                    _launchUrl("https://www.linkedin.com/in/jakub--stepien/"),
+                child: IconTile(
+                  showText: ResponsiveLayout.isDesktop(context),
+                  icon: FontAwesomeIcons.linkedin,
+                  iconColor: Colors.white,
+                  text: "Linkedin",
+                  textColor: Colors.white,
+                  backgroundColor: Colors.blueAccent,
+                ),
               ),
-              IconTile(
-                showText: ResponsiveLayout.isDesktop(context),
-                icon: FontAwesomeIcons.twitter,
-                iconColor: Colors.white,
-                text: "Twitter",
-                textColor: Colors.white,
-                backgroundColor: Colors.blue,
+              GestureDetector(
+                onTap: () => _launchUrl("https://www.twitter.com/_Stempnio"),
+                child: IconTile(
+                  showText: ResponsiveLayout.isDesktop(context),
+                  icon: FontAwesomeIcons.twitter,
+                  iconColor: Colors.white,
+                  text: "Twitter",
+                  textColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                ),
               ),
-              IconTile(
-                showText: ResponsiveLayout.isDesktop(context),
-                icon: FontAwesomeIcons.github,
-                iconColor: Colors.white,
-                text: "Github",
-                textColor: Colors.white,
-                backgroundColor: Colors.black54,
+              GestureDetector(
+                onTap: () => _launchUrl("https://www.github.com/stempnio"),
+                child: IconTile(
+                  showText: ResponsiveLayout.isDesktop(context),
+                  icon: FontAwesomeIcons.github,
+                  iconColor: Colors.white,
+                  text: "Github",
+                  textColor: Colors.white,
+                  backgroundColor: Colors.black54,
+                ),
               ),
             ],
           ),
