7d781c12ef6126381c7d2302bfbd5f2a14ff987e
jakubstepien <jakub.stepien@student.uj.edu.pl>
2022-11-26 18:42:22 +0100

    add: project extension method for empty projects

--- lib/projects/model/project.dart
+++ lib/projects/model/project.dart
@@ -9,3 +9,16 @@ class Project {
   final Repository repository;
   final String imageAssetName;
 }
+
+extension BlankProjects on Project {
+  static Project blankProject() =>
+      Project(repository: Repository(), imageAssetName: "");
+
+  static List<Project> getBlankProjects(int number) {
+    List<Project> result = [];
+    for (var i = 0; i < number; i++) {
+      result.add(blankProject());
+    }
+    return result;
+  }
+}
